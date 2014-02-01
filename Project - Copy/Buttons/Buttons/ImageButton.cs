54e1a0cfd5e3c567be6457c3da0f4024c4be8ceb
YoruichiShunko <nicolas.carton@epita.fr>
2014-02-01 18:22:14 +0100

    Add my classes
    j'ai ajouté mes classes je vais les implementer

---
+++ Project - Copy/Buttons/Buttons/ImageButton.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ButtonImage
+{
+    class ImageButton
+    {
+        private Texture2D img;
+        private SpriteBatch spriteBatch;
+        private MouseState mouse;
+        private MouseState oldMouse;
+        public bool takingAction = false;
+        public bool clicked = false;
+        Rectangle ImgRectangle;
+        Vector2 ImgLocation;
+        bool clickable = true;
+
+        public ImageButton(SpriteBatch sBatch, Texture2D image, Vector2 textLoc)
+        {
+            spriteBatch = sBatch;
+            img = image;
+
+            ImgLocation = textLoc;
+            ImgRectangle = new Rectangle((int)ImgLocation.X, (int)ImgLocation.Y, (int)img.Height, (int)img.Width);
+        }
+
+        public Texture2D Img
+        {
+            get { return img; }
+            set
+            {
+                img = value;
+                ImgLocation = new Vector2();
+            }
+        }
+        public bool Clickable
+        {
+            get { return clickable; }
+            set
+            {
+                clickable = value;
+                Draw();
+            }
+        }
+        public bool TakingAction()
+        {
+            return takingAction;
+
+        }
+        public int bottom
+        {
+            get
+            {
+                return ImgRectangle.Height + ImgRectangle.Y;
+            }
+        }
+
+        public int top
+        {
+            get { return ImgRectangle.Y; }
+        }
+        public int right
+        {
+            get { return ImgRectangle.X + ImgRectangle.Width; }
+        }
+        public int left
+        {
+            get { return ImgRectangle.X; }
+        }
+
+
+        public void Location(int x, int y)
+        {
+            ImgLocation.X = x;
+            ImgLocation.Y = y;
+        }
+
+        public void Update()
+        {
+            if (!clickable)
+                return;
+
+            mouse = Mouse.GetState();
+
+            takingAction = false;
+
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                if (ImgRectangle.Contains(new Point(mouse.X, mouse.Y)))
+                {
+                    clicked = true;
+                    Console.WriteLine("Clicked ");
+                    takingAction = true;
+                    DrawButton(ImgRectangle);
+                }
+                else
+                {
+                    clicked = false;
+                }
+
+            }
+
+            if (mouse.LeftButton == ButtonState.Released && oldMouse.LeftButton == ButtonState.Pressed && clicked)
+            {
+                Console.WriteLine("Action!");
+                takingAction = true;
+            }
+
+
+            oldMouse = mouse;
+        }
+
+        public void Draw()
+        {
+            if (clickable)
+            {
+                DrawButton(ImgRectangle);
+            }
+        }
+        private void DrawButton(Rectangle boundaries)
+        {
+            float xScale = (boundaries.Width);
+            float yScale = (boundaries.Height);
+            float scale = Math.Min(xScale, yScale);
+
+            //Put the image on location we want
+            Vector2 position = new Vector2();
+            position.X = ((boundaries.Width / 2) + boundaries.X);
+            position.Y = ((boundaries.Height / 2) + boundaries.Y);
+
+            // A bunch of settings where we just want to use reasonable defaults.
+            float rotation = 0.0f;
+            Vector2 spriteOrigin = new Vector2(0, 0);
+            float spriteLayer = 0.0f; // all the way in the front
+            SpriteEffects spriteEffects = new SpriteEffects();
+
+            Color color;
+
+            if (clicked && mouse.LeftButton == ButtonState.Pressed)
+            {
+                color = Color.Silver;
+            }
+            else
+            {
+                color = Color.White;
+            }
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(img, ImgRectangle, null, color, rotation, spriteOrigin, spriteEffects, spriteLayer);
+
+            spriteBatch.End();
+
+        }
+    }
+}
---
+++ Project - Copy/Buttons/Buttons/InterfaceInGame.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace ButtonImage
+{
+    class InterfaceInGame
+    {
+        private SpriteBatch spriteBatch;
+        ImageButton[] buttons;
+        //Texture2D[] imgs;
+        public Texture2D background;
+        public bool menuOn;
+
+        public InterfaceInGame(ImageButton[] buttonArray,/* Texture2D[] im,*/ Texture2D bg, SpriteBatch sBatch)
+        {
+            buttons = buttonArray;
+            //imgs = im;
+            spriteBatch = sBatch;
+            background = bg;
+        }
+
+        public bool MenuOn
+        {
+
+            get { return menuOn; }
+
+            set
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].Clickable = value;
+                    buttons[i].clicked = false;
+                }
+                menuOn = value;
+                Draw(100, 100);
+            }
+        }
+        public void Update()
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Update();
+            }
+
+        }
+
+        public void Draw(int screenwidth, int screenheight)
+        {
+
+            if (!menuOn)
+            {
+                return;
+            }
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(13 * screenwidth / 16, 0, 2 * screenwidth / 8, screenheight), Color.White);
+            spriteBatch.End();
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Clickable = true;
+                buttons[i].Update();
+                buttons[i].Draw();
+            }
+            /*spriteBatch.Begin();
+            for (int i = 0; i < imgs.Length; i++)
+            {
+                  spriteBatch.Draw(imgs[i], new Rectangle(imgs[i]., imgs[i].Width, Color.White);
+            }
+            spriteBatch.End();*/
+        }
+
+        public bool buttonWithIndexPressed(int n)
+        {
+            return buttons[n].clicked && buttons[n].takingAction;
+        }
+
+        private int buttonsTakingAction()
+        {
+            int n = 0;
+            for (int i = 0; i < buttons.Length; i++)
+                if (buttons[i].TakingAction())
+                    n++;
+
+            return n;
+        }
+    }
+}
+
