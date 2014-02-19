da08654678a7717fb8c84f7fad4e0a826af0fe9b
Magnet-Alpha <g.antin@laposte.net>
2014-02-19 12:12:58 +0100

    yolo

--- Project - Copy/Buttons/Buttons/Units/Virus.cs
+++ Project - Copy/Buttons/Buttons/Units/Virus.cs
@@ -15,6 +15,8 @@ namespace Buttons
     class Virus : Unit
     {
         protected double Speed { get; set; }
+        private int dir;
+        private int x;
         public override Etat State
         {
             get
@@ -29,6 +31,8 @@ namespace Buttons
         public Virus(string name, int hp, int attack, int cooldown, Vector2 position, double speed, ContentManager content, CustomSpriteBatch sb, Etat e) : base(name, hp, attack, cooldown, position, content, sb, e)
         {
             this.Speed = speed;
+            dir = 0;
+            x = 0;
             imgs.Add(content.Load<Texture2D>("Sprites\\virus\\virus"));
             imgs.Add(content.Load<Texture2D>("TestSprites\\test attack 1" + this.Name));
             imgs.Add(content.Load<Texture2D>("TestSprites\\test dead 1" + this.Name));
@@ -58,12 +62,14 @@ namespace Buttons
                         float z = this.moving.X;
                         this.moving.X = this.moving.Y;
                         this.moving.Y = -z;
+                        dir = (dir + 1) % 4;
                     }
                     else
                     {
                         float z = this.moving.X;
                         this.moving.X = -this.moving.Y;
                         this.moving.Y = z;
+                        dir = (dir - 1) % 4;
                     }
                 }
             }
@@ -71,7 +77,20 @@ namespace Buttons
 
         public override void StateDrawing(float w, float h)
         {
-            unitbatch.Draw(imgs[img], new Rectangle((int)this.Position.X, (int)this.Position.Y, 32 * (int)w , 32 * (int)h), new Rectangle(0, 0, 132, 133), Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
+            int y;
+            x = (x + 1) % 60;
+            switch (dir)
+            {
+                case 0: y = 3;
+                    break;
+                case 1: y = 1;
+                    break;
+                case 2: y = 2;
+                    break;
+                default: y = 0;
+                    break;
+            }
+            unitbatch.Draw(imgs[img], new Rectangle((int)this.Position.X, (int)this.Position.Y, 32 * (int)w , 32 * (int)h), new Rectangle(((int)x/30) * 132 , y*133, 132, 133), Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
         }
     }
 }
