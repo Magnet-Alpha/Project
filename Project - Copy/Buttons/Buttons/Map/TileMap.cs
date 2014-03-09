c9aab2680171dee58705c676f793e39dfc315949
Kojima13 <Kojima13@live.fr>
2014-03-09 07:06:23 +0100

    Virus route complete
    I have to fix this horrible hole between the sprite of the route...
    And add more details around the map

--- Project - Copy/Buttons/Buttons/Map/TileMap.cs
+++ Project - Copy/Buttons/Buttons/Map/TileMap.cs
@@ -19,14 +19,15 @@ namespace Buttons
 
         public TileMap(int width, int height)
         {
-            MapWidth = 100;
-            MapHeight = 130;
+            MapWidth = 30;
+            MapHeight = 60;
+
             for (int y = 0; y < MapHeight; y++)
             {
                 MapRow thisRow = new MapRow();
                 for (int x = 0; x < MapWidth; x++)
                 {
-                    thisRow.Columns.Add(new MapCell(0)); //Texture générale
+                        thisRow.Columns.Add(new MapCell(0)); //Texture générale
                 }
                 Rows.Add(thisRow);
             }
@@ -69,6 +70,220 @@ namespace Buttons
             Rows[14].Columns[2].AddBaseTile(4);
             Rows[14].Columns[4].AddBaseTile(4);
             Rows[14].Columns[3].AddBaseTile(4);
+
+            Rows[14].Columns[3].AddBaseTile(1);
+            Rows[15].Columns[3].AddBaseTile(3);
+            Rows[16].Columns[3].AddBaseTile(1);
+            Rows[17].Columns[3].AddBaseTile(3);
+            Rows[18].Columns[3].AddBaseTile(1);
+            Rows[19].Columns[3].AddBaseTile(3);
+            Rows[20].Columns[3].AddBaseTile(1);
+            Rows[21].Columns[3].AddBaseTile(3);
+            Rows[22].Columns[3].AddBaseTile(1);
+            Rows[23].Columns[3].AddBaseTile(3);
+            Rows[24].Columns[3].AddBaseTile(1);
+            Rows[25].Columns[3].AddBaseTile(3);
+            Rows[26].Columns[3].AddBaseTile(1);
+            Rows[27].Columns[3].AddBaseTile(3);
+            Rows[28].Columns[3].AddBaseTile(1);
+            Rows[29].Columns[3].AddBaseTile(3);
+            Rows[30].Columns[3].AddBaseTile(1);
+            Rows[31].Columns[3].AddBaseTile(3);
+            Rows[32].Columns[3].AddBaseTile(1);
+            Rows[33].Columns[3].AddBaseTile(3);
+            Rows[34].Columns[3].AddBaseTile(1);
+            Rows[35].Columns[3].AddBaseTile(1);
+            Rows[36].Columns[3].AddBaseTile(1);
+            Rows[37].Columns[3].AddBaseTile(1);
+            Rows[38].Columns[3].AddBaseTile(4);
+            Rows[15].Columns[2].AddBaseTile(2);
+            Rows[17].Columns[2].AddBaseTile(2);
+            Rows[19].Columns[2].AddBaseTile(2);
+            Rows[21].Columns[2].AddBaseTile(2);
+            Rows[23].Columns[2].AddBaseTile(2);
+            Rows[25].Columns[2].AddBaseTile(2);
+            Rows[27].Columns[2].AddBaseTile(2);
+            Rows[29].Columns[2].AddBaseTile(2);
+            Rows[31].Columns[2].AddBaseTile(2);
+            Rows[33].Columns[2].AddBaseTile(2);
+            Rows[35].Columns[2].AddBaseTile(2);
+            Rows[37].Columns[2].AddBaseTile(2);
+
+            Rows[37].Columns[4].AddBaseTile(1);
+            Rows[37].Columns[5].AddBaseTile(1);
+            Rows[37].Columns[6].AddBaseTile(1);
+            Rows[37].Columns[7].AddBaseTile(1);
+            Rows[37].Columns[8].AddBaseTile(1);
+            Rows[37].Columns[9].AddBaseTile(1);
+            Rows[37].Columns[10].AddBaseTile(1);
+            Rows[37].Columns[11].AddBaseTile(1);
+            Rows[37].Columns[12].AddBaseTile(1);
+            Rows[37].Columns[13].AddBaseTile(3);
+            Rows[36].Columns[4].AddBaseTile(1);
+            Rows[36].Columns[5].AddBaseTile(1);
+            Rows[36].Columns[6].AddBaseTile(1);
+            Rows[36].Columns[7].AddBaseTile(1);
+            Rows[36].Columns[8].AddBaseTile(1);
+            Rows[36].Columns[9].AddBaseTile(1);
+            Rows[36].Columns[10].AddBaseTile(1);
+            Rows[36].Columns[11].AddBaseTile(1);
+            Rows[36].Columns[12].AddBaseTile(1);
+            Rows[36].Columns[13].AddBaseTile(1);
+            Rows[35].Columns[4].AddBaseTile(1);
+            Rows[35].Columns[5].AddBaseTile(1);
+            Rows[35].Columns[6].AddBaseTile(1);
+            Rows[35].Columns[7].AddBaseTile(1);
+            Rows[35].Columns[8].AddBaseTile(1);
+            Rows[35].Columns[9].AddBaseTile(1);
+            Rows[35].Columns[10].AddBaseTile(1);
+            Rows[35].Columns[11].AddBaseTile(1);
+            Rows[35].Columns[12].AddBaseTile(1);
+            Rows[35].Columns[13].AddBaseTile(3);
+            Rows[34].Columns[4].AddBaseTile(5);
+            Rows[34].Columns[5].AddBaseTile(5);
+            Rows[34].Columns[6].AddBaseTile(5);
+            Rows[34].Columns[7].AddBaseTile(5);
+            Rows[34].Columns[8].AddBaseTile(5);
+            Rows[34].Columns[9].AddBaseTile(5);
+            Rows[34].Columns[10].AddBaseTile(5);
+            Rows[34].Columns[11].AddBaseTile(5);
+            Rows[34].Columns[12].AddBaseTile(5);
+            Rows[34].Columns[13].AddBaseTile(1);
+            Rows[38].Columns[4].AddBaseTile(4);
+            Rows[38].Columns[5].AddBaseTile(4);
+            Rows[38].Columns[6].AddBaseTile(4);
+            Rows[38].Columns[7].AddBaseTile(4);
+            Rows[38].Columns[8].AddBaseTile(4);
+            Rows[38].Columns[9].AddBaseTile(4);
+            Rows[38].Columns[10].AddBaseTile(4);
+            Rows[38].Columns[11].AddBaseTile(4);
+            Rows[38].Columns[12].AddBaseTile(4);
+            Rows[38].Columns[13].AddBaseTile(4);
+
+            Rows[33].Columns[13].AddBaseTile(3);
+            Rows[32].Columns[13].AddBaseTile(1);
+            Rows[31].Columns[13].AddBaseTile(3);
+            Rows[30].Columns[13].AddBaseTile(1);
+            Rows[29].Columns[13].AddBaseTile(3);
+            Rows[28].Columns[13].AddBaseTile(1);
+            Rows[27].Columns[13].AddBaseTile(3);
+            Rows[26].Columns[13].AddBaseTile(1);
+            Rows[25].Columns[13].AddBaseTile(3);
+            Rows[24].Columns[13].AddBaseTile(1);
+            Rows[23].Columns[13].AddBaseTile(3);
+            Rows[22].Columns[13].AddBaseTile(5);
+            Rows[33].Columns[12].AddBaseTile(2);
+            Rows[31].Columns[12].AddBaseTile(2);
+            Rows[29].Columns[12].AddBaseTile(2);
+            Rows[27].Columns[12].AddBaseTile(2);
+            Rows[25].Columns[12].AddBaseTile(1);
+            Rows[23].Columns[12].AddBaseTile(1);
+
+            Rows[23].Columns[11].AddBaseTile(1);
+            Rows[23].Columns[10].AddBaseTile(1);
+            Rows[23].Columns[9].AddBaseTile(1);
+            Rows[23].Columns[8].AddBaseTile(1);
+            Rows[23].Columns[7].AddBaseTile(2);
+            Rows[24].Columns[12].AddBaseTile(1);
+            Rows[24].Columns[11].AddBaseTile(1);
+            Rows[24].Columns[10].AddBaseTile(1);
+            Rows[24].Columns[9].AddBaseTile(1);
+            Rows[24].Columns[8].AddBaseTile(1);
+            Rows[25].Columns[11].AddBaseTile(1);
+            Rows[25].Columns[10].AddBaseTile(1);
+            Rows[25].Columns[9].AddBaseTile(1);
+            Rows[25].Columns[8].AddBaseTile(1);
+            Rows[25].Columns[7].AddBaseTile(2);
+            Rows[26].Columns[12].AddBaseTile(4);
+            Rows[26].Columns[11].AddBaseTile(4);
+            Rows[26].Columns[10].AddBaseTile(4);
+            Rows[26].Columns[9].AddBaseTile(4);
+            Rows[26].Columns[8].AddBaseTile(4);
+            Rows[22].Columns[12].AddBaseTile(5);
+            Rows[22].Columns[11].AddBaseTile(5);
+            Rows[22].Columns[10].AddBaseTile(5);
+            Rows[22].Columns[9].AddBaseTile(5);
+            Rows[22].Columns[8].AddBaseTile(1);
+
+            Rows[21].Columns[8].AddBaseTile(3);
+            Rows[20].Columns[8].AddBaseTile(1);
+            Rows[19].Columns[8].AddBaseTile(3);
+            Rows[18].Columns[8].AddBaseTile(1);
+            Rows[17].Columns[8].AddBaseTile(3);
+            Rows[16].Columns[8].AddBaseTile(1);
+            Rows[15].Columns[8].AddBaseTile(3);
+            Rows[14].Columns[8].AddBaseTile(1);
+            Rows[13].Columns[8].AddBaseTile(3);
+            Rows[12].Columns[8].AddBaseTile(1);
+            Rows[11].Columns[8].AddBaseTile(1);
+            Rows[10].Columns[8].AddBaseTile(1);
+            Rows[21].Columns[7].AddBaseTile(2);
+            Rows[19].Columns[7].AddBaseTile(2);
+            Rows[17].Columns[7].AddBaseTile(2);
+            Rows[15].Columns[7].AddBaseTile(2);
+            Rows[13].Columns[7].AddBaseTile(2);
+            Rows[11].Columns[7].AddBaseTile(2);
+
+            Rows[9].Columns[7].AddBaseTile(2);
+            Rows[8].Columns[8].AddBaseTile(5);
+            Rows[8].Columns[9].AddBaseTile(5);
+            Rows[8].Columns[10].AddBaseTile(5);
+            Rows[8].Columns[11].AddBaseTile(5);
+            Rows[8].Columns[12].AddBaseTile(5);
+            Rows[8].Columns[13].AddBaseTile(5);
+            Rows[9].Columns[8].AddBaseTile(1);
+            Rows[9].Columns[9].AddBaseTile(1);
+            Rows[9].Columns[10].AddBaseTile(1);
+            Rows[9].Columns[11].AddBaseTile(1);
+            Rows[9].Columns[12].AddBaseTile(1);
+            Rows[9].Columns[13].AddBaseTile(1);
+            Rows[10].Columns[9].AddBaseTile(1);
+            Rows[10].Columns[10].AddBaseTile(1);
+            Rows[10].Columns[11].AddBaseTile(1);
+            Rows[10].Columns[12].AddBaseTile(1);
+            Rows[10].Columns[13].AddBaseTile(1);
+            Rows[11].Columns[9].AddBaseTile(1);
+            Rows[11].Columns[10].AddBaseTile(1);
+            Rows[11].Columns[11].AddBaseTile(1);
+            Rows[11].Columns[12].AddBaseTile(1);
+            Rows[11].Columns[9].AddBaseTile(1);
+            Rows[12].Columns[9].AddBaseTile(4);
+            Rows[12].Columns[10].AddBaseTile(4);
+            Rows[12].Columns[11].AddBaseTile(4);
+            Rows[12].Columns[12].AddBaseTile(4);
+            Rows[12].Columns[13].AddBaseTile(4);
+
+            Rows[8].Columns[13].AddBaseTile(1);
+            Rows[8].Columns[14].AddBaseTile(1);
+            Rows[8].Columns[15].AddBaseTile(1);
+            Rows[9].Columns[13].AddBaseTile(1);
+            Rows[9].Columns[14].AddBaseTile(1);
+            Rows[7].Columns[13].AddBaseTile(1);
+            Rows[7].Columns[14].AddBaseTile(1);
+            Rows[10].Columns[13].AddBaseTile(1);
+            Rows[10].Columns[14].AddBaseTile(1);
+            Rows[10].Columns[15].AddBaseTile(1);
+            Rows[11].Columns[13].AddBaseTile(1);
+            Rows[11].Columns[14].AddBaseTile(1);
+            Rows[12].Columns[13].AddBaseTile(1);
+            Rows[12].Columns[14].AddBaseTile(1);
+            Rows[12].Columns[15].AddBaseTile(1);
+            Rows[13].Columns[13].AddBaseTile(1);
+            Rows[13].Columns[14].AddBaseTile(1);
+            Rows[7].Columns[12].AddBaseTile(2);
+            Rows[9].Columns[12].AddBaseTile(1);
+            Rows[11].Columns[12].AddBaseTile(1);
+            Rows[13].Columns[12].AddBaseTile(2);
+            Rows[6].Columns[13].AddBaseTile(5);
+            Rows[6].Columns[14].AddBaseTile(5);
+            Rows[6].Columns[15].AddBaseTile(5);
+            Rows[7].Columns[15].AddBaseTile(3);
+            Rows[9].Columns[15].AddBaseTile(3);
+            Rows[11].Columns[15].AddBaseTile(3);
+            Rows[13].Columns[15].AddBaseTile(3);
+            Rows[14].Columns[13].AddBaseTile(4);
+            Rows[14].Columns[15].AddBaseTile(4);
+            Rows[14].Columns[14].AddBaseTile(4);
 
 
         }
